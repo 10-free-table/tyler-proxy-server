5c71eb40d5b3c35ef5b97696a1be621884ab810e
T K <tyler@Ts-MacBook-Pro.local>
2020-02-06 10:38:39 -0800

    Initial setup and integration of reviews module

--- server/index.js
+++ server/index.js
@@ -1,16 +1,13 @@
 const express = require('express');
 const path = require('path');
 
-const PORT = 3001;
+const PORT = 3000;
 
 const server = express();
 
 server.use(express.static(path.join(__dirname, '..', 'client', 'public')));
-
-// server.get('/', (req, res) => {
-//   res.send('hello warld');
-// });
+server.use('/:pageId', express.static(path.join(__dirname, '..', 'client', 'public')));
 
 server.listen(PORT, () => {
-  console.log(`server running on port: ${PORT}`);
+  console.log(`proxy server running on port: ${PORT}`);
 });
